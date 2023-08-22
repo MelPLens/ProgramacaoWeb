3bf1c09f28462db284e452de134135ef0e2641fa
MEL PLENS ANGELIS <99374140+MelPLens@users.noreply.github.com>
2023-08-21 22:34:22 -0300

    Delete ex4.js

--- ex4.js
+++
@@ -1,6 +0,0 @@
-//Faça um programa que recebe o nome de uma variável e mostre o <<primeiro>>.<<último nome>>@facens.br
-//Por exemplo:
-
-//A variável nome possui o conteúdo “Edson Martin Feitosa”.
-
-//Deve mostrar edson.feitosa@facens.br
