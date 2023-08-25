66b0ce21dab11512a828bffb9c3a1ea25fdc4c65
MEL PLENS ANGELIS <99374140+MelPLens@users.noreply.github.com>
2023-08-25 19:49:56 -0300

    Delete Js-Lista01 directory

--- Js-Lista01/ex03.js
+++
@@ -1,3 +0,0 @@
-//Faça um programa que mostre a quantidade de caracteres para o texto “Disciplina de Programação para web”.
-const disciplina = "Disciplina de Programação para web";
-console.log("Quantidade de caracteres:", disciplina.length);
--- Js-Lista01/ex07.js
+++
@@ -1,7 +0,0 @@
-//Declare um array com 5 nomes de frutas. 
-//Use um loop para percorrer o array e imprima cada elemento no console.
-
-const frutas = ["Maçã", "Melão", "Laranja", "Kiwi", "Morango"];
-for (let i = 0; i < frutas.length; i++) {
-  console.log(frutas[i]);
-}
