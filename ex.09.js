43adbf945375674c8d64922ce4e99f154e8d8df4
MEL PLENS ANGELIS <99374140+MelPLens@users.noreply.github.com>
2023-08-25 19:52:47 -0300

    Delete ex.09.js

--- ex.09.js
+++
@@ -1,6 +0,0 @@
-//Escreva uma função que receba uma string como argumento e retorne a string invertida.
-function inverterString(str) {
-    return str.split("").reverse().join("");
-  }
-  
-  console.log(inverterString("hello"));
